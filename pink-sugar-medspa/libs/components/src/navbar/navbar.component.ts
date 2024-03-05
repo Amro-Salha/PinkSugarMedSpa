fc23af7c47d6877d9c854ea20aa5ab5793c1793a
Amro Salha <amrosalhasoftware@gmail.com>
2024-03-05 09:35:39 -0500

    Accomodated mobile device displays and removed ionic icons

--- pink-sugar-medspa/libs/components/src/navbar/navbar.component.ts
+++ pink-sugar-medspa/libs/components/src/navbar/navbar.component.ts
@@ -1,14 +1,39 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ViewChild } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'psm-navbar',
   templateUrl: './navbar.component.html',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  protected isMobile: boolean = false;
+
+  ngOnInit(): void {
+    this.isMobileDevice();
+  }
+  protected isMobileDevice() {
+    this.isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+  }
+}
