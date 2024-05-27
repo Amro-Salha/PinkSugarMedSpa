3c0cb8ee78113195ab7be1dbb4112a69cd13d9da
Amro Salha <amrosalhasoftware@gmail.com>
2024-05-26 21:43:28 -0400

    Added animated top banner to landing page

--- pink-sugar-medspa/libs/views/src/landing-page/landing-page.component.ts
+++ pink-sugar-medspa/libs/views/src/landing-page/landing-page.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { gsap } from 'gsap';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'psm-landing-page',
   templateUrl: './landing-page.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LandingPageComponent implements OnInit {
-  ngOnInit(): void {
+export class LandingPageComponent implements AfterViewInit {
+  protected taglines: string[] = [
+    'Cosmetic Aesthetics',
+    'Contemporary Beauty x Advanced Science',
+    'Medical director',
+    'Cosmetic Aesthetics',
+    'Contemporary Beauty x Advanced Science',
+    'Medical director',
+  ];
+  @ViewChild('content', { static: true })
+  protected content?: ElementRef;
+
+  ngAfterViewInit(): void {
     this.pageAnimations();
   }
 
   private pageAnimations() {
+    // logo animations
     gsap
       .timeline({ repeat: -1, yoyo: true })
       .to('.logo', { scale: 1.1, duration: 1 });
@@ -23,6 +36,19 @@ export class LandingPageComponent implements OnInit {
     gsap.from('.logo', {
       duration: 2,
       opacity: 0,
+    });
+
+    // banner animations
+    const contentWidth = this.content?.nativeElement.clientWidth;
+
+    gsap.to(this.content?.nativeElement, {
+      x: -contentWidth / 2,
+      duration: 45,
+      ease: 'linear',
+      repeat: -1,
+      modifiers: {
+        x: (x) => `${parseFloat(x) % (contentWidth / 2)}px`,
+      },
     });
   }
 }
